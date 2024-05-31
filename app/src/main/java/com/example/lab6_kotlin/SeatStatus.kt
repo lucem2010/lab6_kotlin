85399be70e5c75d2c8fd75526638699a176b699f
luc tien <lucd7464@gmail.com>
2024-05-31 20:41:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab6_kotlin/SeatStatus.kt
@@ -0,0 +1,5 @@
+package com.example.lab6_kotlin
+
+enum class SeatStatus {
+    EMPTY, SELECTED, BOOKED, AISLE
+}
---
+++ app/src/main/java/com/example/lab6_kotlin/ui/theme/Seat.kt
@@ -0,0 +1,7 @@
+package com.example.lab6_kotlin.ui.theme
+
+import com.example.lab6_kotlin.SeatStatus
+
+data class Seat (var row: Char, val number: Int, var status:SeatStatus)
+{
+}
